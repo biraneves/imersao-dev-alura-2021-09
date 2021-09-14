e34a5ca8621043f6796dce943570e840afc5ab6c
Ubirajara Neves <ubirajara.neves@usp.br>
2021-09-14 10:41:34 -0300

    Criada a função conversionButtonClick()

--- conversor-de-temperatura/assets/js/base-script.js
+++ conversor-de-temperatura/assets/js/base-script.js
@@ -20,3 +20,27 @@ function fahrenheitParaCelsius(temperaturaFahrenheit) {
     return 5 * (temperaturaFahrenheit - 32) / 9;
 
 }
+
+function conversionButtonClick() {
+
+    var optionSelected = document.querySelector('input[name="convert-temperature"]:checked').value;
+    var returnTemperature = 0.0;
+
+    if (document.getElementById("temperature").value != "") {
+
+        if (optionSelected == "opt-c2f")
+            returnTemperature = celsiusParaFahrenheit(parseFloat(document.getElementById("temperature").value)).toFixed(1) + " ºF";
+        else
+            returnTemperature = fahrenheitParaCelsius(parseFloat(document.getElementById("temperature").value)).toFixed(1) + " ºC";
+    
+        document.getElementById("label-temperature-output").innerHTML = returnTemperature;
+
+    } else {
+
+        document.getElementById("label-temperature-output").innerHTML = "Informe um valor.";
+
+    }
+
+    document.getElementById("label-temperature-output").style.display = "block";
+
+}

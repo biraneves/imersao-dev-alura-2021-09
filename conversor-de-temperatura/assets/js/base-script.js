75f7fec7941c0919601b7e1c30dbe0be868e0888
Ubirajara Neves <ubirajara.neves@usp.br>
2021-09-14 10:29:05 -0300

    Criada a função radioClick()

--- conversor-de-temperatura/assets/js/base-script.js
+++ conversor-de-temperatura/assets/js/base-script.js
@@ -1,1 +1,10 @@
-// Scripts for temperature conversion
+function radioClick() {
+
+    var optionSelected = document.querySelector('input[name="convert-temperature"]:checked').value;
+
+    if (optionSelected == "opt-c2f")
+        document.getElementById("temperature").placeholder = "Informe a temperatura em ºC";
+    else
+        document.getElementById("temperature").placeholder = "Informe a temperatura em ºF";
+
+}
